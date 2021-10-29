b09e9bd7f03dc2d6abff95a04b757d1fe4d127ff
departurr <72740667+departurr@users.noreply.github.com>
2021-10-30 02:08:59 +0700

    Uploaded Needed files..

---
+++ Project for GDSC/js/jquery-func.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	
+	$(".movie-image").hover(function(){
+		$(this).find(".play").show();
+
+	},
+	function()
+	{
+		$(this).find(".play").hide();
+	});
+
+
+	$(".blink").focus(function() {
+            if(this.title==this.value) {
+                this.value = '';
+            }
+        })
+        .blur(function(){
+            if(this.value=='') {
+                this.value = this.title;                    
+			}
+		});
+});
+
+$(window).load(function () {
+    $(".trigger_popup_fricc").click(function(){
+       $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+});
+
